cef2fbbf600c881b89da473942f17d5ca9b36e93
Khalil Benazzouz <k.benazzouz@b3gtech.com>
2023-02-28 19:26:55 +0100

    Remove unused methods

--- Flight.WEBAPP/Controllers/HomeController.cs
+++ Flight.WEBAPP/Controllers/HomeController.cs
@@ -62,55 +62,6 @@ namespace Flight.WEBAPP.Controllers
             return View(flightModel);
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-
-        public ActionResult create()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public ActionResult create(FlightModel model)
-        {
-
-            var responseAPI = _response.GetAPIResponse(out string pResponse);
-            var flightData = JsonConvert.DeserializeObject<FlightResponse>(pResponse);
-            List<Datum> flightList = new List<Datum>();
-            foreach (var item in flightData.data.Where(x=>x.flight_date.Equals(DateTime.Now.ToString("yyyy-MM-dd"))).ToList())
-            {
-                flightList.Add(item);
-            }
-            _caching.GetSetAsyncList(DateTime.Now.ToString("yyyy-MM-dd"), flightList);
-
-            var getList = _caching.GetSetAsyncList(DateTime.Now.ToString("yyyy-MM-dd"), flightList);
-            if (getList.Result.Count() != 0)
-            {
-
-            }
-
-
-            string message = "Created the record successfully";
-
-            // To display the message on the screen
-            // after the record is created successfully
-            ViewBag.Message = message;
-
-            // write @Viewbag.Message in the created
-            // view at the place where you want to
-            // display the message
-            return View();
-        }
-
-
         [HttpGet] // Set the attribute to Read
         public ActionResult Read(string numVol)
         {
